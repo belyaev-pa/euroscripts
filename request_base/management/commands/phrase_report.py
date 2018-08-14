8aad4499b4e960645868d4ea25607d94292447cc
sqim_op <sqim_op@gmail.com>
2018-08-14 22:05:41 +0300

    update

--- request_base/management/commands/phrase_report.py
+++ request_base/management/commands/phrase_report.py
@@ -17,29 +17,33 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         phrase_list = Phrase.objects.filter(phrase_len__lte=4).order_by('phrase_len')#prefetch_related('link_set__word')
         print(phrase_list.count())
+        t = time.time()
         with open(self.csv_output_file, 'w+', newline='', encoding='Windows-1251') as output_file:
             word_writer = csv.writer(output_file, delimiter=';',
                                     quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            #for phrase in phrase_list:
-                #phrase_word_list = phrase.link_set.values_list('word', flat=True)
-                #phrase_with_phrase = Phrase.objects.filter(link__word__in=phrase_word_list).annotate(
-                #    num_words=Count('link__words')).filter(num_words=len(phrase_word_list))
-                #phrase_count = phrase_with_phrase.count()
-                #print(phrase.phrase)
-                #print(phrase_count)
-                #phrase_frequency = phrase_with_phrase.aggregate(Sum('frequency'))
-                #print(phrase_frequency)
-                #word_writer.writerow([phrase.phrase, phrase_count, phrase_frequency])
             from_begin, till_end = 0, 1000
+            print("start - {}".format(str(time.time() - t)))
             while True:
                 phrase_list_pack = Phrase.objects.filter(phrase_len__lte=4).order_by('phrase_len')[from_begin:till_end]
-                print(phrase_list_pack.query)
-
                 if phrase_list_pack.exists():
                     for phrase in phrase_list_pack:
-                        phrase_word_list = phrase.link_set.values_list('word', flat=True)
-                        phrase_with_phrase = Phrase.objects.filter(link__word__in=phrase_word_list).annotate(
-                            num_words=Count('link__words')).filter(num_words=len(phrase_word_list))
+                        phrase_word_list = phrase.link_set.values_list('word_id', flat=True)
+                        if len(phrase_word_list) == 1:
+                            phrase_with_phrase = Phrase.objects.filter(link__word_id=phrase_word_list[0])
+                        elif len(phrase_word_list) == 2:
+                            phrase_with_phrase = Phrase.objects.filter(link__word_id=phrase_word_list[0]
+                                                             ).filter(link__word_id=phrase_word_list[1])
+                        elif len(phrase_word_list) == 3:
+                            phrase_with_phrase = Phrase.objects.filter(link__word_id=phrase_word_list[0]
+                                                             ).filter(link__word_id=phrase_word_list[1]
+                                                             ).filter(link__word_id=phrase_word_list[1])
+                        elif len(phrase_word_list) == 4:
+                            phrase_with_phrase = Phrase.objects.filter(link__word_id=phrase_word_list[0]
+                                                             ).filter(link__word_id=phrase_word_list[1]
+                                                             ).filter(link__word_id=phrase_word_list[2]
+                                                             ).filter(link__word_id=phrase_word_list[3])
+                        else:
+                            continue
                         phrase_count = phrase_with_phrase.count()
                         phrase_frequency = phrase_with_phrase.aggregate(Sum('frequency'))
                         word_writer.writerow([phrase.phrase, phrase_count, phrase_frequency])
@@ -47,5 +51,5 @@ class Command(BaseCommand):
                     break
                 from_begin += 1000
                 till_end += 1000
-                print(from_begin)
+                print("{} items - {}".format(from_begin, str(time.time() - t)))
 
